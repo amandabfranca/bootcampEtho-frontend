75af87b44fba3fa5a9505e04f88cf9ec4d431722
amandabfranca <francabs.amanda@gmail.com>
2021-12-20 22:22:42 -0300

    Correção do login.screen

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Button from "../../components/buttons/button/button.component";
-import {Grid} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 
 export default function Login(){
     return(
         <>
+        <Container>
         <Grid container justifyContent={'center'}>
             <Grid item xs={4}>
             <Button primary>Entrar</Button>
             </Grid>   
         </Grid>
+         </Container>
         </>
+
 
     )
 }
