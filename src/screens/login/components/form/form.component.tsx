c6d4edeecc8d25bd70bcd3e6e0f59515a655758b
amandabfranca <francabs.amanda@gmail.com>
2021-12-22 21:19:41 -0300

    adicionando validações na tela principal

--- src/screens/login/components/form/form.component.tsx
+++ src/screens/login/components/form/form.component.tsx
@@ -1,24 +1,68 @@
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 import InputText from "../../../../components/inputs/input-text/input-text.component";
 import Button from "../../../../components/buttons/button/button.component";
+import * as yup from 'yup'
+import {ErrorMessage} from "./form.types";
+import {ErrorDescription} from "./form.styled";
 
-
+const errorInitial = ''
 
 export default function Form() {
     const [data, setData] = useState( { email: '', password: ''})
+    const [error, setError] = useState(errorInitial)
+      
+    const resetError = useCallback(
+        () => setError (errorInitial),
+        []
+    )
 
-    const handleChange = (event: any) => setData (prevState => ({
-        ...prevState,
-        [event.target.name]: event.target.value,
-    }))
+    const handleChange = useCallback(
+        (event: any) => setData(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+        })), 
+        [setData]
+    )
     
-    console.log(data, 'data')
+    const validation = useCallback(
+        async() => {
+            const schema = yup.object().shape({
+                email: yup.string().required(ErrorMessage.Required).email(ErrorMessage.EmailBadFormat),
+                password: yup.string().required(ErrorMessage.Required),
+            })
+
+            try {
+                await schema.validate(data)
+                resetError()
+    
+
+                return true
+            }catch (error) {               
+                // @ts-ignore
+                setError(error.errors[0])
+    
+                return false
+            }
+
+        },
+        [data, setError]
+
+    )
+
+    const onSubmit = useCallback(
+        async () => {
+            await validation()
+        },
+        [validation]
+
+    )
 
     return(
         <>
             <InputText type={'text'} placeholder={'E-mail'} name={'email'} onChange={handleChange}/>
             <InputText type={'password'} placeholder={'Senha'} name={'password'} onChange={handleChange}/>
-            <Button primary>Entrar</Button>  
+            <ErrorDescription>{error}</ErrorDescription>
+            <Button primary onClick={onSubmit}>Entrar</Button>  
         </>
         
     )
---
+++ src/screens/login/components/form/form.types.ts
@@ -0,0 +1,5 @@
+export enum ErrorMessage{
+
+Required = 'Um campos não foram preenchidos',
+EmailBadFormat = 'O e-mail não é válido',
+}
