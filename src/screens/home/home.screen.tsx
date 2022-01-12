6625537cafc9af5b391c6ba27e37f1543ec36935
amandabfranca <francabs.amanda@gmail.com>
2022-01-12 19:09:30 -0300

    Deploy do frontend na nuvem

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import login from '../../assets/icons/logo.svg';
+import {LogoHome, Wrapper} from "../home/home.styled"
+import {Container, Grid} from "@mui/material";
 
 export default function Home() {
-    return <h1>Home</h1>
-}
+    return(
+    <>
+        <Container>
+                <Wrapper container justifyContent={'left'}>
+                    <Grid item xs={12} justifyContent ={'left'}>
+                        <Grid container justifyContent={'center'}>
+                            <LogoHome src={login} alt={'Netflix Logo'}/>
+                            <h1>Quem está assistindo?</h1>
+                        </Grid>   
+                        </Grid>
+                </Wrapper>
+            </Container>
+
+    </>
+)}
---
+++ src/screens/home/home.styled.ts
@@ -0,0 +1,17 @@
+import styled  from "styled-components";
+import {Grid} from "@mui/material";
+
+
+export const Wrapper = styled(Grid)`
+min-height: 100vh;
+align-items: left;
+`
+export const LogoHome = styled.img`
+margin: 0 0 128px;
+top: 21px;
+left: 390px;
+width: 1140px;
+height: 77px;
+
+
+`
